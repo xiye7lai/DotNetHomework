300f6849b4fd87b3559429e84bd580665d1baa06
xiye7lai <1692823208@qq.com>
2021-03-02 21:16:39 +0800

    homework1 finished

---
+++ HomeWork_1/Windows窗体应用-计算器/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                int result= int.Parse(textBox1.Text) + int.Parse(textBox2.Text);
+                label2.Text = Convert.ToString(result);
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                int result = int.Parse(textBox1.Text) - int.Parse(textBox2.Text);
+                label2.Text = Convert.ToString(result);
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                int result = int.Parse(textBox1.Text) * int.Parse(textBox2.Text);
+                label2.Text = Convert.ToString(result);
+            }
+            else
+            {
+                int result = int.Parse(textBox1.Text) / int.Parse(textBox2.Text);
+                label2.Text = Convert.ToString(result);
+            }
+        }
+    }
+}
---
+++ HomeWork_1/控制台应用-计算器/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入第一个数字");
+            int a = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("请输入运算符");
+            string s = Console.ReadLine();
+            Console.WriteLine("请输入第二个数字");
+            int b = Int32.Parse(Console.ReadLine());
+            int result = 0;
+            switch (s)
+            {
+                case "+" : result = a + b;
+                    break;
+                case "-": result = a - b;
+                    break;
+                case "*": result = a * b;
+                    break;
+                case "/": result = a / b;
+                    break;
+                default:
+                    break;
+            }
+            Console.WriteLine($"运算结果为{result}");
+        }
+    }
+}
