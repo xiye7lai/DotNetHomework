3c9b82e1cf1f4accf3e1fe50f77c6992be102613
xiye7lai <1692823208@qq.com>
2021-03-19 10:28:01 +0800

    HomeWork4

---
+++ HomoWork_4/HomoWork_4_1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace HomoWork_4_1
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+        public Node<T> Head
+        {
+            get => head;
+        }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else{
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T> action)
+        {
+            Node<T> n;
+            n = head;
+            while (n != null)
+            {
+                action(n.Data);
+                n = n.Next;
+            }
+        }
+    }
+    public class Test
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> intlist = new GenericList<int>();
+            for (int x = 0; x < 10; x++)
+            {
+                intlist.Add(x);
+            }
+            int max = int.MinValue;
+            int min = int.MaxValue;
+            int sum = 0;
+            intlist.ForEach(n => Console.Write($"{n},"));
+            intlist.ForEach(n => max = n > max ? n : max);
+            intlist.ForEach(n => min = n < min ? n : min);
+            intlist.ForEach(n => sum += n);
+            Console.WriteLine($"\n最大值为{max},最小值为{min},和为{sum}");
+        }
+    }
+}
---
+++ HomoWork_4/HomoWork_4_2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace HomoWork_4_2
+{
+    //Alarm事件类
+    public class AlarmEventArgs : EventArgs
+    {
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+        public int Second { get; set; }
+        public string AlarmContent { get; set; }
+        public AlarmEventArgs(int hour,int minute,int second,string alarmContent)
+        {
+            Hour = hour;
+            Minute = minute;
+            Second = second;
+            AlarmContent = alarmContent;
+        }
+    }
+    //声明委托
+    public delegate void AlarmHandler(object sender, AlarmEventArgs args);
+    class Clock
+    {
+        private int hour;
+        private int minute;
+        private int second;
+        private int alarm_hour;
+        private int alarm_minute;
+        private int alarm_second;
+        private string alarm_content;
+
+        public Clock(int hour,int minute,int second)
+        {
+            this.hour = hour;
+            this.minute = minute;
+            this.second = second;
+        }
+
+        public event AlarmHandler OnAlarm;
+
+        public void Alarm()
+        {
+            Console.WriteLine(this.alarm_content);
+            AlarmEventArgs args = new AlarmEventArgs(this.alarm_hour,this.alarm_minute,this.alarm_second,this.alarm_content);
+            OnAlarm(this, args);
+        }
+        public void SetAlarm(int hour, int minute, int second, string alarmContent)
+        {
+            this.alarm_hour = hour;
+            this.alarm_minute = minute;
+            this.alarm_second = second;
+            this.alarm_content = alarmContent;
+        }
+        public void StartClock()
+        {
+            //TODO
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
